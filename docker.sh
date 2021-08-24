feca4aa5f4026c2783589dd1d5335ea48d746c4d
luxu <2463219846@qq.com>
2021-08-24 16:05:46 +0800

    修改docker sh脚本

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 mvn clean package  docker:build
 echo "当前docker 镜像："
 docker images
+echo "停止镜像："
+docker stop myspring
+echo "删除 镜像："
+docker rm myspring
 echo "启动容器----->"
-docker run -p 8001:8001 -d dockerspringboot
+docker run --name myspring -p 8001:8001 -d dockerspringboot
 echo "启动服务成功！"
