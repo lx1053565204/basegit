82a59f8324ea11f20ab2e821ee9eeb2f4081798b
luxu <2463219846@qq.com>
2021-08-24 17:02:19 +0800

    脚本优化

--- docker.sh
+++ docker.sh
@@ -1,12 +1,13 @@
-mvn clean package  docker:build
-echo "当前docker 镜像："
-docker images | grep dockerspringboot
 echo "停止容器："
 docker stop myspring
 echo "删除容器："
 docker rm myspring
 echo "删除镜像："
 docker rmi dockerspringboot
+echo "打包："
+mvn clean package  docker:build
+echo "当前docker 镜像："
+docker images | grep dockerspringboot
 echo "启动容器----->"
 docker run --name myspring -p 8001:8001 -d dockerspringboot
 echo "启动服务成功！"
